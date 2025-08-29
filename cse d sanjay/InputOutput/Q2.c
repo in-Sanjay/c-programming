ca7f9c0af5f8966ae95f73ee19265c8838361bf9
in-Sanjay <ppakkiyaraj25@gmail.com>
2025-08-29 12:03:26 +0530

    Add files via upload

---
+++ cse d sanjay/InputOutput/Q2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	char a[50];
+	scanf("%s",a);// book
+	printf("%s",a);// book
+}
---
+++ cse d sanjay/InputOutput/Q3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("my cse d student");
+	return 0;
+}
